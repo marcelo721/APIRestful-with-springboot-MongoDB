1e10c0e41bde0afdcc37bebaec37a71ab8b209d2
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-11 18:00:39 -0300

    created class UserDTO

--- src/main/java/com/marcelohsousa/courseSpringboot/resources/UserResources.java
+++ src/main/java/com/marcelohsousa/courseSpringboot/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.marcelohsousa.courseSpringboot.resources;
 
 
+import com.marcelohsousa.courseSpringboot.dto.UserDTO;
 import com.marcelohsousa.courseSpringboot.entities.User;
 import com.marcelohsousa.courseSpringboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -22,9 +24,10 @@ public class UserResources {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
 
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 }
