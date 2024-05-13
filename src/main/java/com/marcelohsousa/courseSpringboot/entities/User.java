7c0a3fd6d2c545772f1d801a84dfabbfae6fda62
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-13 14:22:13 -0300

    Referencing the user's posts

--- src/main/java/com/marcelohsousa/courseSpringboot/entities/User.java
+++ src/main/java/com/marcelohsousa/courseSpringboot/entities/User.java
@@ -1,9 +1,12 @@
 package com.marcelohsousa.courseSpringboot.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -14,6 +17,10 @@ public class User  implements Serializable {
     private String id;
     private String name;
     private String email;
+
+
+    @DBRef(lazy = true)
+    private List<Post> posts = new ArrayList<>();
 
 
     public User() {
@@ -47,6 +54,10 @@ public class User  implements Serializable {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
     }
 
     @Override
